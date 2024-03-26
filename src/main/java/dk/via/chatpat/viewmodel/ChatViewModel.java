6b8f9fbd984d8c88345d874904ee7bf77b17b7e8
Dimitar Nizamov <dimitur2204@gmail.com>
2024-03-27 00:17:48 +0200

    Add handling of new chat member joining

--- src/main/java/dk/via/chatpat/viewmodel/ChatViewModel.java
+++ src/main/java/dk/via/chatpat/viewmodel/ChatViewModel.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,16 +20,20 @@ public class ChatViewModel implements PropertyChangeListener {
     private final ChatModel model;
     private SimpleStringProperty message;
     private SimpleStringProperty chatArea;
-    public ChatViewModel(ChatModel model) {
+    private ObservableList<User> chatters;
+
+    public ChatViewModel(ChatModel model) throws IOException {
         this.model = model;
         this.message = new SimpleStringProperty();
         this.chatArea = new SimpleStringProperty();
+        this.chatters = FXCollections.observableList(model.getChatters());
         model.addPropertyChangeListener(this);
     }
 
     public SimpleStringProperty messageProperty() {
         return message;
     }
+
     public SimpleStringProperty chatAreaProperty() {
         return chatArea;
     }
@@ -38,28 +43,39 @@ public class ChatViewModel implements PropertyChangeListener {
     }
 
     public ObservableList<User> getChatters() {
-        try {
-            ArrayList<User> chatters = model.getChatters();
-            return FXCollections.observableList(chatters);
-        } catch (Exception e) {
-            throw new RuntimeException("Server communication error", e);
+        return this.chatters;
+    }
+
+    private void addChatter(User user) {
+        this.chatters.add(user);
+    }
+
+    private void appendToChat(String message) {
+        if (chatArea.get() == null) {
+            chatArea.set(message);
+            return;
         }
+        chatArea.set(chatArea.get() + message);
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         Platform.runLater(() -> {
-            if (evt.getPropertyName().equals("message_received")) {
-                if (evt.getNewValue() instanceof Message msg) {
+            String propName = evt.getPropertyName();
+            Object newValue = evt.getNewValue();
+            if (propName.equals("message_received")) {
+                if (newValue instanceof Message msg) {
                     Date date = new Date(msg.getTimestamp());
                     SimpleDateFormat sdf = new SimpleDateFormat("h:mm");
                     sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
                     String formattedDate = sdf.format(date);
-                    if (chatArea.get() == null) {
-                        chatArea.set(formattedDate + " - " + msg.getSender() + ": " + msg.getContent() + "\n");
-                        return;
-                    }
-                    chatArea.set(chatArea.get() + formattedDate + " - " + msg.getSender() + ": " + msg.getContent() + "\n");
+                    appendToChat(formattedDate + " - " + msg.getSender() + ": " + msg.getContent() + "\n");
+                }
+            }
+            if (propName.equals("new_chatter")) {
+                if (newValue instanceof User user) {
+                    addChatter(user);
+                    appendToChat(user + " has just joined the chat." + "\n");
                 }
             }
         });
--- src/main/java/dk/via/chatpat/viewmodel/ViewModelFactory.java
+++ src/main/java/dk/via/chatpat/viewmodel/ViewModelFactory.java
@@ -2,11 +2,13 @@ package dk.via.chatpat.viewmodel;
 
 import dk.via.chatpat.model.ChatModel;
 
+import java.io.IOException;
+
 public class ViewModelFactory {
     private final ChatViewModel chatViewModel;
     private final LoginViewModel loginViewModel;
 
-    public ViewModelFactory(ChatModel chatModel) {
+    public ViewModelFactory(ChatModel chatModel) throws IOException {
         this.chatViewModel = new ChatViewModel(chatModel);
         this.loginViewModel = new LoginViewModel(chatModel);
     }
